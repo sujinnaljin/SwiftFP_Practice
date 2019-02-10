e0b2646299fde61234fdddcab88652dcb5218281
강수진 <rkdthd0403@gmail.com>
2019-02-10 23:36:21 +0900

    Weather Forecast - nonFP

--- FizzBuzz/FizzBuzz/main.swift
+++ FizzBuzz/FizzBuzz/main.swift
@@ -7,7 +7,7 @@
 //
 
 /*
- FizzBuzz (non-FP)
+ FizzBuzz
  FizzBuzz 프로그램은 1 ~ 100 까지의 숫자를 출력하되, 3으로 나누어 떨어진다면 fizz, 5로 나누어 떨어진다면 buzz, 3과 5 둘 다 나누어 떨어지면 fizzbuzz를 출력하는 프로그램 입니다.
  */
 
@@ -21,7 +21,7 @@ func loop(min : Int, max: Int, do f: (Int)->Void){
     Array(min...max).forEach(f)
 }
 
-loop(min: 1, max: 100, do: { print(fizzbuzz($0))})
+//loop(min: 1, max: 100, do: { print(fizzbuzz($0))})
 
 
 /*
@@ -74,4 +74,77 @@ enum Result : String{
     case wrong = "Wrong"
 }
 
-countingLoop(inputAndCheck(generateNum(1, 100)), corrected)
+//countingLoop(inputAndCheck(generateNum(1, 100)), corrected)
+
+/*
+ Weather Forecast
+ 특정지역의 날씨를 일별로 조회하여 표시합니다.
+ 
+ 검색어로 지역명을 조회할 수 있습니다. (예를들어 “york 검색어는 York 와 New York 두 지역을 검색할 수 있습니다.)
+ 특정 지역의 날씨를 조회합니다.
+ 날짜별로 날씨현황, 최고/저 온도를 표시합니다.
+ 지역 및 날씨 조회는 www.metaweather.com 에서 제공하는 Open API를 사용합니다.
+ 네트워크로 조회한 결과로 반환되는 JSON을 파싱하여 사용합니다.
+ API Document - https://www.metaweather.com/api/
+ */
+
+struct Location: Codable {
+    let title: String
+    let location_type: String
+    let woeid: Int
+    let latt_long: String
+}
+
+struct WeatherInfo: Codable {
+    let consolidated_weather: [Weather]
+}
+
+struct Weather: Codable {
+    let weather_state_name: String
+    let wind_direction_compass: String
+    let applicable_date: String
+    let min_temp: Float
+    let max_temp: Float
+    let the_temp: Float
+    let wind_speed: Float
+    let wind_direction: Float
+    let air_pressure: Float
+    let humidity: Float
+    let visibility: Float
+    let predictability: Float
+}
+
+let query = "seoul"
+let locQueryUrl = URL(string: "https://www.metaweather.com/api/location/search?query=\(query)")!
+
+if let locData = try? Data(contentsOf: locQueryUrl) {
+    if let locations = try? JSONDecoder().decode([Location].self, from: locData) {
+        
+        for location in locations {
+            print("[\(location.title)]")
+            
+            let woeid = location.woeid
+            let weatherUrl = URL(string: "https://www.metaweather.com/api/location/\(woeid)")!
+            
+            if let weatherData = try? Data(contentsOf: weatherUrl) {
+                if let weatherInfo = try? JSONDecoder().decode(WeatherInfo.self, from: weatherData) {
+                    
+                    let weathers = weatherInfo.consolidated_weather
+                    for weather in weathers {
+                        let state = weather.weather_state_name.padding(toLength: 15,
+                                                                       withPad: " ",
+                                                                       startingAt: 0)
+                        let forecast = String(format: "%@: %@ %2.2f°C ~ %2.2f°C",
+                                              weather.applicable_date,
+                                              state,
+                                              weather.max_temp,
+                                              weather.min_temp)
+                        print(forecast)
+                    }
+                }
+            }
+            
+            print("")
+        }
+    }
+}
